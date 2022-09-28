21d8917c68ddd505df6fda1c342348efefb0585f
osamaalek <osamaalek99@gmail.com>
2022-09-28 22:10:46 +0300

    Exit kiosk mode

--- app/src/main/java/com/osamaalek/kiosklauncher/ui/HomeFragment.kt
+++ app/src/main/java/com/osamaalek/kiosklauncher/ui/HomeFragment.kt
@@ -4,27 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.osamaalek.kiosklauncher.R
+import com.osamaalek.kiosklauncher.util.KioskUtil
 
 class HomeFragment : Fragment() {
 
     private lateinit var fabApps: FloatingActionButton
+    private lateinit var imageButtonExit: ImageButton
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_home, container, false)
 
         fabApps = v.findViewById(R.id.floatingActionButton)
+        imageButtonExit = v.findViewById(R.id.imageButton_exit)
 
         fabApps.setOnClickListener {
             requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainerView, AppsListFragment())
-                .commit()
+                .replace(R.id.fragmentContainerView, AppsListFragment()).commit()
+        }
+
+        imageButtonExit.setOnClickListener {
+            KioskUtil.stopKioskMode(requireActivity())
         }
 
         return v
