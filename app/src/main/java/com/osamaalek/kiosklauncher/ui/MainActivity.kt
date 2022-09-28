ba425932e41dcf71a06a52d1a7be8446ec1889ce
osamaalek <osamaalek99@gmail.com>
2022-09-28 22:09:57 +0300

    start kiosk mode

--- app/src/main/java/com/osamaalek/kiosklauncher/ui/MainActivity.kt
+++ app/src/main/java/com/osamaalek/kiosklauncher/ui/MainActivity.kt
@@ -3,16 +3,19 @@ package com.osamaalek.kiosklauncher.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.osamaalek.kiosklauncher.R
+import com.osamaalek.kiosklauncher.util.KioskUtil
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        KioskUtil.startKioskMode(this)
     }
 
     override fun onBackPressed() {
-        if (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) is AppsListFragment)
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainerView, HomeFragment()).commit()
+        if (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) is AppsListFragment) supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, HomeFragment()).commit()
     }
 }
